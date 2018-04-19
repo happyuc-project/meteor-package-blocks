6b4c545c75d0b4be50c2ca0fdea22cbcb76dade0
ldcc <ldcc@live.com>
2018-04-19 16:13:26 +0800

    update dep webu

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "happyuc:blocks",
   summary: "Provides informations about the current and last 50 blocks",
-  version: "1.0.10",
+  version: "1.0.11",
   git: "http://github.com/happyuc-project/meteor-package-blocks"
 });
 
@@ -9,8 +9,6 @@ Package.onUse(function(api) {
   api.versionsFrom("1.0");
   api.use("underscore", ["client", "server"]);
   api.use("mongo", ["client", "server"]);
-
-  // api.use('frozeman:persistent-minimongo@0.1.3', 'client');
   api.use("happyuc:webu@1.0.0", ["client", "server"]);
   api.export(["HucBlocks"], ["client", "server"]);
   api.addFiles("blocks.js", ["client", "server"]);
