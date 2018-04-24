af54a018bea9c7f2d83e2ebf95599dfbf175dac4
ldcc <ldcc@live.com>
2018-04-24 18:34:04 +0800

    update dep: webu

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name   : 'happyuc:blocks',
   summary: 'Provides informations about the current and last 50 blocks',
-  version: '1.1.3',
+  version: '1.1.4',
   git    : 'http://github.com/happyuc-project/meteor-package-blocks',
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use('underscore', ['client', 'server']);
   api.use('mongo', ['client', 'server']);
-  api.use('happyuc:webu@1.0.5', ['client', 'server']);
+  api.use('happyuc:webu@1.0.8', ['client', 'server']);
   api.export(['HucBlocks'], ['client', 'server']);
   api.addFiles('blocks.js', ['client', 'server']);
 });
